4ce351df9daced5a5d8c0a0c190193a6a4037e97
HussainAli507 <hussainali507@icloud.com>
2023-06-01 02:16:33 -0700

    Add files via upload

---
+++ src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png";
+
+const Banner = () => {
+    return (
+        <div className="hero-banner">
+            <div className="content">
+                <div className="text-content">
+                    <h1>SALES</h1>
+                    <p>
+                    We see our customers as invited
+                     guests to a party, and we are the hosts. It’s 
+                     our job every day to make every important aspect
+                      of the customer experience a little bit better.
+                    </p>
+                    <div className="ctas">
+                        <div className="banner-cta">Read More</div>
+                        <div className="banner-cta v2">Shop Now</div>
+                    </div>
+                </div>
+                <img className="banner-img" src={BannerImg} />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
